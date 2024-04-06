5ac1c80a45a887b1aa38146a669860c26f01d06f
ysc12451 <ysc12451@163.com>
2024-04-06 11:00:52 -0500

    Update limma-voom.R

--- limma-voom.R
+++ limma-voom.R
@@ -1,4 +1,5 @@
 library(edgeR)
+library(ggplot2)
 
 #' Apply DE analysis using limma-voom
 #'
@@ -7,13 +8,17 @@ library(edgeR)
 #' @param contrfml a string like "A-B" to make contrast between A and B
 #' @param p_thres a p value threshold for significance
 #' @param cutoff cutoff for filtering low expressed genes
-#' @return a list: 1. toptab: topTable for the result, 2. de_genes: DE gene names, de_genes_bool: a boolean vector for whether a gene is DE, 4 volcano_plot
+#' @param batch batch
+#' @param thres_type string "adjp" or "p", using adjusted p value or p value for significance
+#' @param adj_method the FDR methods
+#' @return a list: 1. toptab: topTable for the result, 2. de_genes: DE gene names, 3 volcano_plot
 #' @export
 
-DE_limmavoom <- function(counts, group, contrfml, p_thres=0.05, cutoff=1, batch=NULL){
+DE_limmavoom <- function(counts, group, contrfml, p_thres=0.05, cutoff=1, batch=NULL
+                         , thres_type="adjp", adj_method = "BH"){
   # 1. data input
   # Create DGEList object
-  gene_names <- rownames(counts)
+  # gene_names <- rownames(counts)
   d0 <- DGEList(counts)
   
   # 2. Preprocessing
@@ -22,7 +27,13 @@ DE_limmavoom <- function(counts, group, contrfml, p_thres=0.05, cutoff=1, batch=
   
   # Filter low-expressed genes
   drop <- which(apply(cpm(d0), 1, max) < cutoff)
-  d <- d0[-drop,] 
+  if (length(drop) != 0) {
+    d <- d0[-drop,] 
+  } else {
+    d <- d0
+  }
+  
+  print(paste0(nrow(d), " out of ", nrow(d0), " genes left after filtering low-expressed genes"))
   
   # 3. Voom transformation and calculation of variance weights
   # Specify the model to be fitted.  We do this before using voom since voom uses variances of the model residuals (observed - fitted)
@@ -39,7 +50,7 @@ DE_limmavoom <- function(counts, group, contrfml, p_thres=0.05, cutoff=1, batch=
   # - A linear model is fitted to the log2 CPM for each gene, and the residuals are calculated
   # - A smoothed curve is fitted to the sqrt(residual standard deviation) by average expression (see red line in plot above)
   # - The smoothed curve is used to obtain weights for each gene and sample that are passed into limma along with the log2 CPMs.
-  y <- voom(d, mm, plot = T)
+  y <- voom(d, mm, plot = FALSE)
   
   # 4. Fitting linear models in limma
   # lmFit fits a linear model using weighted least squares for each gene:
@@ -55,7 +66,7 @@ DE_limmavoom <- function(counts, group, contrfml, p_thres=0.05, cutoff=1, batch=
   tmp <- eBayes(tmp)
   
   # What genes are most differentially expressed?
-  top.table <- topTable(tmp, sort.by = "P", n = Inf)
+  top.table <- topTable(tmp, sort.by = "P", adjust.method = adj_method, n = Inf)
   top.table$genes <- rownames(top.table)
   # head(top.table, 20)
   # - logFC: log2 fold change of I5.9/I5.6 
@@ -67,19 +78,49 @@ DE_limmavoom <- function(counts, group, contrfml, p_thres=0.05, cutoff=1, batch=
   
   # DE gene index by adjusted p value
   # Filter significant genes
-  de_genes <- rownames(top.table)[top.table$adj.P.Val < 0.05]
-  de_genes_bool <- gene_names %in% de_genes
+  if (thres_type == "adjp") {
+    de_genes <- rownames(top.table)[top.table$adj.P.Val < p_thres]
+  }
+  else if (thres_type == "p") {
+    de_genes <- rownames(top.table)[top.table$P.Value < p_thres]
+  }
+  else {
+    print(paste0("Wrong thres_type", thres_type, ". It should be adjp or p."))
+    return(NULL)
+  }
   
   # make volcano plot
-  significant_genes <- subset(toptable, adj.P.Val < p_thres)
+  significant_genes <- subset(top.table, adj.P.Val < p_thres)
   
-  volcano_plot <- volcano_plot <- ggplot(toptable, aes(x = logFC, y = -log10(adj.P.Val))) +
+  volcano_plot <- volcano_plot <- ggplot(top.table, aes(x = logFC, y = -log10(adj.P.Val))) +
     geom_point(color = "black", alpha = 0.5) +
     geom_text(data = significant_genes, aes(label = genes), hjust = -0.1, vjust = -0.1, size = 3) + # Add text labels for significant genes
     geom_vline(xintercept = 0, linetype = "dashed", color = "blue") + # Add a vertical line at logFC = 0
-    geom_hline(yintercept = significance_threshold, linetype = "dashed", color = "red") + # Add a horizontal line at significance threshold
+    geom_hline(yintercept = p_thres, linetype = "dashed", color = "red") + # Add a horizontal line at significance threshold
     labs(x = "logFC", y = "-log10(adj.P.Val)", title = "Volcano Plot") +
     theme_minimal()
   
-  return(list(toptable=top.table, de_genes = de_genes, de_genes_bool = de_genes_bool, volcano_plot=volcano_plot))
+  return(list(toptable=top.table, de_genes = de_genes, volcano_plot=volcano_plot))
+}
+
+calculate_metrics <- function(confusion_matrix) {
+  TP <- confusion_matrix["DE", "DE"]
+  FP <- confusion_matrix["Not DE", "DE"]
+  TN <- confusion_matrix["Not DE", "Not DE"]
+  FN <- confusion_matrix["DE", "Not DE"]
+  
+  sensitivity <- TP / (TP + FN)
+  specificity <- TN / (TN + FP)
+  accuracy <- (TP + TN) / sum(confusion_matrix)
+  balanced_accuracy <- (sensitivity + specificity) / 2
+  NPV <- TN / (TN + FN)
+  PPV <- TP / (TP + FP)
+  precision <- TP / (TP + FP)
+  recall <- sensitivity
+  
+  metrics <- c(sensitivity = sensitivity, specificity = specificity, accuracy = accuracy,
+               balanced_accuracy = balanced_accuracy, NPV = NPV, PPV = PPV,
+               precision = precision, recall = recall)
+  
+  return(metrics)
 }
